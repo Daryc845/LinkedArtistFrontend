163e7758f6fdfa3fcdd0c35dd554b5edfc3bb33a
Edison <edison.gutierrez01@uptc.edu.co>
2025-11-19 15:18:59 -0500

    feat: fixed api gateway url

--- src/app/auth/services/login.service.ts
+++ src/app/auth/services/login.service.ts
@@ -8,7 +8,7 @@ import { LoginResponse } from '../models/responses/login.responses';
   providedIn: 'root'
 })
 export class LoginService {
-  private apiUrl = 'http://localhost:8080/auth/login';
+  private apiUrl = 'https://partyst-api-gateway.onrender.com/auth/login';
 
   constructor(private http: HttpClient) { }
 
